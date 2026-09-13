1b43431f6391c4b436fb7aa0e7f186016f7633f6
hassannoor230 <hassannoor2309@gmail.com>
2026-09-13 23:00:15 +0500

    commit

--- utils/check-user.js
+++
@@ -1,6 +0,0 @@
-﻿const User = require("./models/User");
-(async () => {
-  const u = await User.findOne({ email: "emma.jones@example.com" }).select("-passwordHash");
-  console.log(JSON.stringify(u, null, 2));
-  process.exit(0);
-})().catch(e => { console.error(e); process.exit(1); });
